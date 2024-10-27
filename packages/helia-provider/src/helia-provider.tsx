8afe391b3d124a3582b610aeaf215a23f3273eef
taylor hulsmans <thulsmans.1133@gmail.com>
2024-10-27 04:51:59 -0700

    debug

--- packages/helia-provider/src/helia-provider.tsx
+++ packages/helia-provider/src/helia-provider.tsx
@@ -1,40 +1,25 @@
 import { createHelia, type Helia } from 'helia'
-import { type Libp2p, type ServiceMap } from 'libp2p'
+import { type Libp2p } from 'libp2p'
 import React, { createContext, useEffect, useState, type ReactNode } from 'react'
 
-
 interface HeliaContextProps {
-  helia: HeliaLibp2p | null
+  helia: Helia | null
 }
-
-
-export interface HeliaLibp2p extends Helia {
-  libp2p: Libp2pPubsub
-}
-
-type PubsubServiceMap = ServiceMap & {
-  message: string;
-}
-
-type Libp2pPubsub = Libp2p<PubsubServiceMap>
-
 
 const HeliaContext = createContext<HeliaContextProps | undefined>(undefined)
-// make libp2p an optional parameter 
 const HeliaProvider: React.FC<{ children: ReactNode, libp2p?: Libp2p }> = ({ children, libp2p }) => {
-  const [heliaInstance, setHeliaInstance] = useState<HeliaLibp2p | null>(null)
+  const [heliaInstance, setHeliaInstance] = useState<Helia | null>(null)
   useEffect(() => {
     void (async () => {
       if (heliaInstance != null) {
         return
       }
-      let helia;
+      let helia
       if (libp2p == null) {
         helia = await createHelia()
       } else {
         helia = await createHelia({ libp2p })
       }
-      console.log('helia', helia)
       setHeliaInstance(helia)
     })()
 
@@ -61,7 +46,7 @@ const HeliaProvider: React.FC<{ children: ReactNode, libp2p?: Libp2p }> = ({ chi
   )
 }
 
-const useHelia = (): HeliaLibp2p | null => {
+const useHelia = (): Helia | null => {
   const context = React.useContext(HeliaContext)
   if (context === undefined) {
     throw new Error('useHelia must be used within a HeliaProvider')
--- packages/helia-provider/src/usePubsubRoom.ts
+++ packages/helia-provider/src/usePubsubRoom.ts
@@ -1,39 +1,36 @@
 import { useEffect, useState, useCallback, useContext } from 'react'
-import { type Helia } from 'helia'
-import { type Libp2p } from 'libp2p'
 import { useHelia } from './helia-provider.js'
 
 const DEFAULT_OPTIONS = {
   pollInterval: 1000
 }
 
-export const usePubsubRoom = ({topic='default', options=DEFAULT_OPTIONS}= {}) => {
-  const helia = useHelia() 
+export const usePubsubRoom = ({
+  topic = 'default',
+  options = DEFAULT_OPTIONS
+} = {}): void => {
+  const helia = useHelia()
   const [peers, setPeers] = useState<string[]>([])
   const [connections, setConnections] = useState<Object>({})
-  const [messages, setMessages] = useState<[]>([])
-
-  const updatePeerList = useCallback(async () => {
-
-  }, [])
+  const [messages, setMessages] = useState<Object[]>([])
 
 
   useEffect(() => {
-    if (helia) {
-      helia.libp2p.addEventListener('connection:open', updatePeerList)
-      helia.libp2p.addEventListener('connection:close', updatePeerList)
+    if (helia != null) {
+      helia.libp2p.addEventListener('connection:open', () => {})
+      helia.libp2p.addEventListener('connection:close', () => {})
       helia.libp2p.addEventListener('self:peer:update', () => {})
-      helia.libp2p.addEventListener('message', (event) => {
+      helia.libp2p.services.pubsub.addEventListener('message', (event: any) => {
         const topic = event.detail.topic
         const message = event.detail.data
-        setMessages(old => [...old, {topic, message}])
+        setMessages(old => [...old, { topic, message } ])
       })
 
       return () => {
-        helia.libp2p.removeEventListener('connection:open', updatePeerList)
-        helia.libp2p.removeEventListener('connection:close', updatePeerList)
+        helia.libp2p.removeEventListener('connection:open', () => {})
+        helia.libp2p.removeEventListener('connection:close', () => {})
         helia.libp2p.removeEventListener('self:peer:update', () => {})
-        helia.libp2p.removeEventListener('message', (event) => {})
+        helia.libp2p.services.pubsub.removeEventListener('message', (event: any) => {})
       }
 
     }
@@ -41,7 +38,7 @@ export const usePubsubRoom = ({topic='default', options=DEFAULT_OPTIONS}= {}) =>
 
 
   setInterval(() => {
-    if (helia ) {
+    if (helia != null) {
       const peerList = helia.libp2p.services.pubsub.getSubscribers(topic)
       setPeers(peerList)
     }
